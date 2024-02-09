dc9416b6508489ce394cfa6d0072a696d0ffaf79
Zohair-coder <zohair.ul.hasan@gmail.com>
2024-02-09 12:31:36 -0500

    added multithreading

--- plan_of_study_scraper.py
+++ plan_of_study_scraper.py
@@ -1,7 +1,8 @@
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from collections import defaultdict
-import re
 from helpers import send_request
 from bs4 import BeautifulSoup
+import os
 
 
 def scrape() -> dict:
@@ -16,12 +17,24 @@ def scrape() -> dict:
     anchor_tags = list(filter(lambda l: not l["href"].startswith("#") and l["href"] != "/accelerateddegreeprograms/" and l["href"] != "/certificates/", anchor_tags))
 
     data = {}
-    for i, anchor_tag in enumerate(anchor_tags):
-        link = base_url + anchor_tag["href"] + "#sampleplanofstudybstext"
+    futures = {}
+    num_cores = os.cpu_count()
+
+    with ThreadPoolExecutor(max_workers=num_cores) as executor:
+        for anchor_tag in anchor_tags:
+            link = base_url + anchor_tag["href"] + "#sampleplanofstudybstext"
+            major = anchor_tag.text
+            future = executor.submit(scrape_page, link)
+            futures[future] = major
         
-        major = anchor_tag.text
-        data[major] = scrape_page(link)
-        print(f"({i+1}/{len(anchor_tags)}) Scraped {major}")
+        for future in as_completed(futures):
+            major = futures[future]
+            
+            try:
+                data[major] = future.result()
+                print(f"Scraped {major}")
+            except Exception as e:
+                print(f"Failed to scrape {major}: {e}")
     
     return data
         
@@ -31,26 +44,10 @@ def scrape_page(url: str) -> dict:
 
     soup = BeautifulSoup(page.content, 'html.parser')
 
-    pattern = re.compile(r'sampleplans?ofstudy.*container')
-    div_tag = soup.find('div', id=pattern)
-
-    if not div_tag:
-        pattern = re.compile(r'.*Plans? of Study.*', re.IGNORECASE)
-        h2_tag = soup.find('h2', string=pattern)
-        if not h2_tag:
-            return {}
-        div_tag = h2_tag.parent
-
-
-    # Example: "4 year, 1 co-op"
-    pattern = re.compile(r"(\w+[- ](?:Years?|YR)[.,]*\s*\w*\s*Co-?ops?\s*(?:\(?Fall\/Winter\)?|\(?Spring\/Summer\)?\s?Cycle)?)|((Fall Winter|Spring Summer) co-op cycle)", re.IGNORECASE)
-    plan_names = div_tag.find_all(string=pattern)
-
     tables = soup.find_all("table", {"class": "sc_plangrid"})
 
     data = {}
     for i, table in enumerate(tables):
-        table = tables[i]
         years = table.find_all("tr", {"class": "plangridyear"})
     
         years_data = defaultdict(list)
@@ -80,7 +77,7 @@ def scrape_page(url: str) -> dict:
         
             years_data[year_text] = term_data
         
-        plan_name = plan_names[i].text if i < len(plan_names) else f"Plan {i+1}"
-        data[plan_name.replace("&nbsp;", " ").replace("\u00a0", " ").strip()] = years_data
+        plan_name = f"Plan {i+1}"
+        data[plan_name] = years_data
     
     return data
